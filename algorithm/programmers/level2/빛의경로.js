a02e9fdb2b11ca896c8d77cb0725b585e5f161f8
yun sungmin <smy07207@gmail.com>
2021-10-28 22:49:04 +0900

    10.28

--- algorithm/programmers/level2/빛의경로.js
+++ algorithm/programmers/level2/빛의경로.js
@@ -1,8 +1,17 @@
 //https://programmers.co.kr/learn/courses/30/lessons/86052
 
 function solution(grid) {
-	var answer = [];
-	return answer;
+	let data = [];
+	for (let i = 0; i < grid.length; i++) {
+		let ta = [];
+		for (let j = 0; j < grid[i].length; j++) {
+			let tmp = {};
+			tmp.type = grid[i][j];
+			ta.push(tmp);
+		}
+		data.push(ta);
+	}
+	console.log(data);
 }
 
-console.log(solution(["SL", "LR"]));
+console.log(solution(["SL", "LR"])); //16
