6b7159f88abe76dbe435bb389d8d91c0863254b5
yun sungmin <smy07207@gmail.com>
2021-10-31 23:32:41 +0900

    10.31

--- algorithm/programmers/level2/빛의경로.js
+++ algorithm/programmers/level2/빛의경로.js
@@ -2,16 +2,27 @@
 
 function solution(grid) {
 	let data = [];
+	let answer = [];
 	for (let i = 0; i < grid.length; i++) {
 		let ta = [];
 		for (let j = 0; j < grid[i].length; j++) {
 			let tmp = {};
 			tmp.type = grid[i][j];
+			tmp.up = 0;
+			tmp.down = 0;
+			tmp.left = 0;
+			tmp.right = 0;
 			ta.push(tmp);
 		}
 		data.push(ta);
 	}
-	console.log(data);
+
+	const travle = (start, now) => {
+		if (start === 1) {
+			answer.push(now);
+			return;
+		}
+	};
 }
 
 console.log(solution(["SL", "LR"])); //16
---
+++ algorithm/programmers/level2/프린터.js
@@ -0,0 +1,6 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42587?language=javascript
+
+function solution(priorities, location) {
+	var answer = 0;
+	return answer;
+}
