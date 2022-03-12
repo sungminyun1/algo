a608c781f41ef523615852e84a16ea51e407e8c9
sungminyun1 <smy07207@gmail.com>
2022-03-12 20:50:26 +0900

    03.12

--- algorithm/boj/silver/easy.js
+++ algorithm/boj/silver/easy.js
@@ -1,30 +1,11 @@
+const readline = require('readline');
+const std = readline.createInterface({
+    input : process.stdin,
+    output : process.stdout,
+})
 
-function main(num){
-    let count = 99;
-    const check = (num)=>{
-        let arr = String(num).split('');
-        let dif = Number(arr[0]) - Number(arr[1]);
-        for(let i =0; i<arr.length-1; i++){
-            if(Number(arr[i]) - Number(arr[i+1]) !== dif){
-                return false;
-            }
-        }
-        return true;
-    }
 
-    if(num < 100){
-        return num;
-    }
-    for(let i = 100; i<=num; i++){
-        if(check(i)){
-            count++;
-        }
-    }
-
-    return count;
-}
-const fs = require('fs');
-const input = fs.readFileSync('dev/stdin').toString().trim();
-console.log(main(Number(input)))
-console.log(main(210))
-
+std.on('line',(line) =>{
+    console.log(line);
+    std.close();
+}).on('close',()=>process.exit())
