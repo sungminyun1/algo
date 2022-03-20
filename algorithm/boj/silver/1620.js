dc92d03daae50a4133d19f01c849ad40bce84b61
sungminyun1 <smy07207@gmail.com>
2022-03-20 22:52:24 +0900

    03.20

--- algorithm/boj/silver/1620.js
+++ algorithm/boj/silver/1620.js
@@ -30,21 +30,23 @@ Raichu
 3
 Pidgey
 Kakuna`.toString().trim().split('\n')
+// const fs = require('fs');
+// const input = fs.readFileSync('dev/stdin').toString().trim().split('\n')
 const [list_num, q_num] = input.shift().split(' ').map(val => +val);
-const po_list = [];
+const po_map =new Map();
 const q_list = [];
 for(let i =0; i<list_num + q_num; i++){
     if(i<list_num){
-        po_list.push(input[i])
+        po_map.set(''+i,input[i])
+        po_map.set(input[i],i)
     }else{
         q_list.push(input[i])
     }
 }
-main(list_num,q_num,po_list,q_list)
+main(list_num,q_num,po_map,q_list)
 
-function main(list_num,q_num,po_list,q_list){
-    console.log(list_num)
-    console.log(q_num)
-    console.log(po_list)
-    console.log(q_list)
+function main(list_num,q_num,po_map,q_list){
+    for(let i =0; i<q_num; i++){
+        console.log(po_map.get(q_list[i]))
+    }
 }
