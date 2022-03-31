a37bad6c94e59908ccccc3e6c4133c251b40bd5e
sungminyun1 <smy07207@gmail.com>
2022-03-31 22:54:30 +0900

    03.31

---
+++ algorithm/leetcode/dep_138.js
@@ -0,0 +1,29 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, next, random) {
+ *    this.val = val;
+ *    this.next = next;
+ *    this.random = random;
+ * };
+ */
+
+/**
+ * @param {Node} head
+ * @return {Node}
+ */
+var copyRandomList = function (head) {
+  let arr = [];
+  let now = head;
+
+  const make = (now) => {
+    if (!now) {
+      return null;
+    }
+    arr.unshift({
+      val: now.val,
+      next: make(now.next),
+      random: arr[now.random],
+    });
+  };
+  make(now);
+};
