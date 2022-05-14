955923a5e29a7bc74a79d26e43ea2b9b694f1695
sungminyun1 <smy07207@gmail.com>
2022-05-15 02:29:29 +0900

    05.15

--- algorithm/boj/silver/6603.js
+++ algorithm/boj/silver/6603.js
@@ -1,29 +1,30 @@
 const input = `7 1 2 3 4 5 6 7
 8 1 2 3 5 8 13 21 34
 0`.split('\n').map(val=>val.split(' ').map(Number));
+// const fs = require('fs');
+// const input = fs.readFileSync('dev/stdin').toString().trim().split('\n').map(val=>val.split(' ').map(Number));
 
 const consoleRes = [];
 for(let i =0; i<input.length-1; i++){
     main(input[i])
 }
+console.log(consoleRes.join('\n\n'))
+
+
 function main(Arr){
     const res = [];
     Arr.shift();
 
-    const make = (arr,now) =>{
-        if(!arr.length){
+    const make = (idx, now) =>{
+        if(now.length >= 6){
             res.push(now.join(' '));
             return;
         }
-
-        for(let i =0; i< arr.length; i++){
-            let t_val = arr[i]
-            let t_arr = [...arr];
-            t_arr.splice(i,1);
-            make(t_arr,[...now,t_val]);
+        for(let i =idx; i<Arr.length; i++){
+            make(i +1, [...now,Arr[i]]);
         }
     }
 
-    make(Arr,[])
-    console.log(res)
+    make(0,[])
+    consoleRes.push(res.join('\n'));
 }
