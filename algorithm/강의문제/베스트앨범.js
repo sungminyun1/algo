326bdb89d1e0e84b080706376979ed5b09270f14
sungminyun1 <smy07207@gmail.com>
2022-06-24 00:06:53 +0900

    06.22

--- algorithm/강의문제/베스트앨범.js
+++ algorithm/강의문제/베스트앨범.js
@@ -10,7 +10,23 @@ function solution(genres, plays) {
         }
     }
 
-    console.log(map)
+    const result = []
+    //Object.keys(map).sort((a,b)=>map[b][0]- map[a][0])
+    let keys = Object.keys(map).sort((a,b)=>map[b][0]- map[a][0])
+    Object.keys(map).sort((a,b)=>map[b][0]- map[a][0]).map(val => map[val][1].sort((a,b)=>{
+        if(a !== b){
+            return plays[b] - plays[a];
+        }else{
+            return a - b
+        }
+    }).map((val2,idx) => {
+
+        if(idx<2){
+            result.push(val2)
+            return val2
+        }
+    }))
+    return result
 }
 
 console.log(solution(["classic", "pop", "classic", "classic", "pop"],[500, 600, 150, 800, 2500]	))
